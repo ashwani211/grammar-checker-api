9790305c97fcf5cd5797bdd1b0c798b5f8b399d8
Ashwani Kumar Singh <ashwanikumarsingh.varanasi@gmail.com>
2023-08-28 08:31:31 +0530

    simple api

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-# pip install -U git+https://github.com/PrithivirajDamodaran/Gramformer.git
-# pip install dill
-# pip install spacy
-# pip install flask
-# pip install torch
-
 from flask import Flask, request, jsonify
 from gramformer import Gramformer
 import torch
@@ -26,26 +20,11 @@ gf = Gramformer(models = 1, use_gpu=False) # 1=corrector, 2=detector
 
 PATH = "/content/sample_data/gf.pth"
 
-# # save file
-# with open(PATH, 'wb') as f:
-#     dill.dump(gf, f)
-
-# # open file
-# with open(PATH, 'rb') as f:
-#     gf_inference = dill.load(f)
-
 influent_sentences = [
     "Hello, I am a goof person",
     "eat you're vegetables",
     "it's your's"
 ]
-
-# for influent_sentence in influent_sentences:
-#     corrected_sentences = gf.correct(influent_sentence)
-#     print("[Input] ", influent_sentence)
-#     for corrected_sentence in corrected_sentences:
-#       print("[Correction] ",corrected_sentence)
-#     print("-" *100)
 
 @app.route("/api/<sentence>")
 def translate(sentence):
@@ -57,25 +36,9 @@ def translate(sentence):
   return jsonify(toreturn)
 
 
-
-
-
-
-
-
-
-# # -----------------------------------------
-# from flask import Flask, request, jsonify
-
-# app = Flask(__name__)
-
 @app.route("/")
 def home():
   return jsonify("Hello World")
-
-# @app.route("/translate/<data>")
-# def translate(data):
-#   return jsonify("translatedString"+data)
 
 if __name__ == "__main__":
   app.run(debug=True)
